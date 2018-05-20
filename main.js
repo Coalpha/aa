4b9ec68d1ea5d982f4a6e402247a2918f202bb68
Acolyte <arcanehood@gmail.com>
2018-05-20 13:17:23 -0700

    So many images

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const canven = {
   fiveground: document.getElementById('fiveground'),
 };
 const ctx = canven.map(_.getContext`2d`);
-const Class =
 let currentFillStyle = '#000000';
 const setFillStyle = (context, color) => {
   if (color !== currentFillStyle) {
@@ -34,76 +33,22 @@ const canvasSize = (width, height) => {
     v.style.height = py;
   });
 };
-// const drawImage =
-// Classes
+// Loaders
 function imageLoader(src) {
   const i = new Image();
   i.src = src;
-  return i;
+  return new Promise((res, rej) => {
+    i.onload = () => res(i);
+    i.onerror = () => rej(`Loading ${src} failed!`);
+  });
 };
-function imageLoader(src, count) {
-  const obj = {};
-  obj.frames = count.times(i => img(`assets/${url}${`${i + 1}`.prefix(0, 4)}.png`));
-  obj.frameCount = count;
-  obj.i = null;
+function SpriteLoader(src, count) {
+  return Promise.all(count.times(i => imageLoader(`assets/${src}${`${i + 1}`.prefix(0, 4)}.png`)));
 }
-const SpriteLoader = C.class('Sprite', {
-  constructor(url, count) {
-
-  },
-});
-const Location = C.class('Location', {
-  constructor(locationName, onDraw = () => undefined) {
-    this.image = img(`assets/locations/${locationName}.png`);
-    this.onDraw = onDraw;
-  },
-  draw() {
-    ctx.background.clearRect(0, 0, adjustedWidth, adjustedHeight);
-    ctx.background.drawImage(this.image, 0, 0, adjustedWidth, adjustedHeight);
-    return this.onDraw();
-  },
-});
-const Chara = C.class('Character', {
-  constructor(obj) {
-    this.foo;
-  },
-});
-const Sound = C.class('Sound Object', {
-  constructor(locationName) {
-    this.elem = document.createElement('audio');
-    this.elem.src = `assets/${locationName}`;
-    audioContainer.appendChild(this.elem);
-  },
-  setTime(n) {
-    this.elem.currentTime = n;
-  },
-  play() {
-    return this.elem.play();
-  },
-  playFromBeginning() {
-    this.setTime(0);
-    return this.play();
-  },
-  pause() {
-    this.elem.pause();
-  },
-});
-const Music = Sound.extend('Music Object', {
-  constructor(locationName) {
-    super.constructor(`music/${locationName}`);
-  },
-});
-const SFX = Sound.extend('Sound Effects Object', {
-  constructor(locationName) {
-    super.constructor(`sfx/sfx-${locationName}.wav`);
-  },
-});
 // Action
 ctx.forEach(v => v.imageSmoothingEnabled = false);
 // No Anti Aliasing
 canvasSize(adjustedWidth, adjustedHeight);
 const loc = Object.create(null);
 const sound = Object.create(null);
-sound.blipFemale = SFX('blipfemale');
-loc.courtroomHall = Location('courtroomHall');
-const pw = Sprite('characters/phoenix/bird', 21);
+const pw = SpriteLoader('characters/phoenix/phoenix-document(b)', 21);
